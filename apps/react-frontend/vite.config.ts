7bbc6147f4cfc402de2408e54fbddec272e67756
KubaDavid <jakub.david@resonect.cz>
2025-06-22 22:37:27 +0200

    feat: init frontend

--- apps/react-frontend/vite.config.ts
+++ apps/react-frontend/vite.config.ts
@@ -4,21 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      // Proxy API requests to the Adonis backend
-      '/api': {
-        target: 'http://localhost:33891',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-      // Proxy SSE requests to the Adonis backend
-      '/__transmit': {
-        target: 'http://localhost:33891',
-        changeOrigin: true,
-        ws: true,
-      }
-    }
-  }
 })
